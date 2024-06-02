032a493278789ce528399cdd022463b5ea8b1320
Akshay B <beatle02@gmail.com>
2024-06-02 13:34:12 -0400

    VV Imp Fix (stop whole tree rerender)

--- src/utils/vdom/vdom-lib.js
+++ src/utils/vdom/vdom-lib.js
@@ -214,12 +214,12 @@ function createElement(node) {
 
 function changed(node1, node2) {
   return (
-    node1 == node2 ||
+    // node1 != node2 ||
     typeof node1 !== typeof node2 ||
     // (typeof node1 === "string" && node1 !== node2) ||
     (!node1?.type && node1 !== node2) ||
-    node1?.type !== node2?.type ||
-    (node1.props && node1.props.forceUpdate)
+    node1?.type !== node2?.type
+    // || (node1?.props && node1.props.forceUpdate)
   );
 }
 
