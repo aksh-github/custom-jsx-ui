630154735943e1155468080947ed10885f22b5bc
Akshay B <beatle02@gmail.com>
2024-08-25 14:38:10 -0400

    new router impl

---
+++ src/utils/router-v2.jsx
@@ -0,0 +1,181 @@
+import { atom } from "./simple-state";
+import { onCleanup, onMount, h } from "./vdom/vdom-lib";
+
+console.log(
+  "check: https://github.com/nanojsx/nano/blob/master/src/components/router.ts"
+);
+console.log(
+  "check: https://codesandbox.io/s/build-own-react-router-v4-mpslz , https://www.youtube.com/watch?v=1knvu0a3k0w"
+);
+
+const historyPush = (path) => {
+  window.history.pushState({}, null, path);
+  //   instances.forEach((instance) => instance.forceUpdate());
+  window.dispatchEvent(new PopStateEvent("navigate"));
+};
+
+const historyReplace = (path) => {
+  window.history.replaceState({}, null, path);
+  //   instances.forEach((instance) => instance.forceUpdate());
+  window.dispatchEvent(new PopStateEvent("navigate"));
+};
+
+const matchPath = (pathname, options) => {
+  const { exact = false, path } = options;
+
+  if (!path) {
+    return {
+      path: null,
+      url: pathname,
+      isExact: true,
+    };
+  }
+
+  const match = new RegExp(`^${path}`).exec(pathname);
+
+  if (!match) return null;
+
+  const url = match[0];
+  const isExact = pathname === url;
+
+  if (exact && !isExact) return null;
+
+  return {
+    path,
+    url,
+    isExact,
+  };
+};
+
+// const [currentPath, setCurentPath] = atom(window.location.pathname);
+
+// export function Route() {
+//   //   const currentPath = atom(window.location.pathname);
+
+//   const navigate = (abcd) => {
+//     console.log(window.location.pathname, abcd);
+//     if (currentPath() !== window.location.pathname)
+//       setCurentPath(window.location.pathname);
+//   };
+
+//   onMount(() => {
+//     window.addEventListener("popstate", navigate);
+//     window.addEventListener("navigate", navigate);
+//   });
+
+//   onCleanup(() => {
+//     window.removeEventListener("popstate", navigate);
+//     window.removeEventListener("navigate", navigate);
+//   });
+
+//   return (props) => {
+//     const { path, exact, component, render } = props;
+
+//     const match = matchPath(window.location.pathname, { path, exact });
+
+//     if (!match) return null;
+
+//     if (component) return h(component, { match });
+
+//     console.log(props);
+
+//     if (render) return render({ match });
+
+//     return null;
+//   };
+// }
+
+export function LinkV2({ to, replace }) {
+  const handleClick = (event) => {
+    event.preventDefault();
+    replace ? historyReplace(to) : historyPush(to);
+  };
+
+  return ({ to, children }) => {
+    return (
+      <a href={to} onClick={handleClick}>
+        {children}
+      </a>
+    );
+  };
+}
+
+export function Router() {
+  const [currPath, setCurrPath] = atom(window.location.pathname);
+  let onRouteChange;
+  const navigate = (abcd) => {
+    console.log(window.location.pathname, abcd);
+    if (currPath() !== window.location.pathname) {
+      setCurrPath(window.location.pathname);
+      onRouteChange(matchPath(currPath(), {}));
+    }
+  };
+
+  return {
+    init: (cb) => {
+      window.addEventListener("popstate", navigate);
+      window.addEventListener("navigate", navigate);
+      onRouteChange = cb || (() => {});
+      onRouteChange(matchPath(currPath(), {}));
+    },
+    cleanup: () => {
+      window.removeEventListener("popstate", navigate);
+      window.removeEventListener("navigate", navigate);
+    },
+  };
+}
+
+// export const SimpleSwitch = (pmain) => {
+//   const currentPath = atom(window.location.pathname);
+
+//   console.log(pmain);
+
+//   const navigate = (abcd) => {
+//     console.log(window.location.pathname, abcd);
+//     if (currentPath.get() !== window.location.pathname)
+//       currentPath.set(window.location.pathname);
+//   };
+
+//   onMount(() => {
+//     window.addEventListener("popstate", navigate);
+//     window.addEventListener("navigate", navigate);
+//   });
+
+//   onCleanup(() => {
+//     window.removeEventListener("popstate", navigate);
+//     window.removeEventListener("navigate", navigate);
+//   });
+
+//   return (props) => {
+//     console.log(props);
+//     const { cond, children } = props;
+
+//     let defaultc = null;
+//     let match = null;
+
+//     const selected = children?.find((c) => {
+//       console.log(c);
+//       const { value } = c;
+
+//       if (value.path === undefined) defaultc = c;
+
+//       match = matchPath(window.location.pathname, {
+//         path: value.path,
+//         exact: value.exact,
+//       });
+//       // return mp ? true : false;
+//       return value.path === window.location.pathname;
+//     });
+
+//     console.log(selected);
+
+//     return (selected || defaultc)?.value?.render({ match });
+//   };
+// };
+
+// SimpleSwitch.Case = () => {
+//   return (props) => {
+//     const { path, render } = props;
+//     return { render, path };
+//   };
+// };
---
+++ src/vdom-app/RouterApp.jsx
@@ -0,0 +1,142 @@
+// import { SimpleSwitch } from "../compos/Switch";
+import { LinkV2, Router } from "../utils/router-v2";
+import { atom, registerCallback, state } from "../utils/simple-state";
+import {
+  mount,
+  h,
+  forceUpdate,
+  onMount,
+  onCleanup,
+} from "../utils/vdom/vdom-lib";
+
+const Home = () => () => <h2>Home</h2>;
+const About = () => () => <h2>About</h2>;
+const Topic =
+  () =>
+  ({ topicId }) =>
+    <h3>{topicId}</h3>;
+
+const Topics = (p) => {
+  const items = [
+    { name: "Props v. State", slug: "props-v-state" },
+    { name: "Rendering with React", slug: "rendering" },
+    { name: "Components", slug: "components" },
+  ];
+  // console.log(p);
+
+  return (props) => {
+    // console.log(curPath.get());
+    const { basepath, match } = props;
+
+    return (
+      <div>
+        <h2>Topics</h2>
+        <ul>
+          {items.map(({ name, slug }) => (
+            <li key={name}>
+              <LinkV2 to={`${basepath}/${slug}`}>{name}</LinkV2>
+            </li>
+          ))}
+        </ul>
+        {/* {items.map(({ name, slug }) => (
+          <Route
+            key={name}
+            path={`${match.path}/${slug}`}
+            render={() => <Topic topicId={name} />}
+          />
+        ))}
+        <Route
+          exact
+          path={match.url}
+          render={() => <h3>Please select a topic.</h3>}
+        /> */}
+        {/* {(() => {
+          const item = items.find(({ name, slug }) => {
+            console.log(curPath.get(), url, slug);
+            return curPath.get()?.url === `${url}/${slug}`; //curPath.get()?.url.endsWith(slug);
+          });
+
+          console.log(item);
+          return item ? <Topic topicId={item.name} /> : null;
+        })()} */}
+        {<Topic topicId={match.url} />}
+      </div>
+    );
+  };
+};
+
+function App() {
+  const [curPath, setCurPath] = state(window.location.pathname);
+
+  const onRouteChange = (newPath) => {
+    console.log(newPath);
+    setCurPath(newPath);
+  };
+  let routeHandler = Router();
+
+  onMount(() => {
+    routeHandler.init(onRouteChange);
+  });
+
+  onCleanup(() => {
+    routeHandler.cleanup();
+  });
+
+  return () => (
+    <div>
+      <ul>
+        <li>
+          <LinkV2 to="/">Home</LinkV2>
+        </li>
+        <li>
+          <LinkV2 to="/about">About</LinkV2>
+        </li>
+        <li>
+          <LinkV2 to="/topics">Topics</LinkV2>
+        </li>
+      </ul>
+
+      <hr />
+
+      {(() => {
+        console.log("this works 25aug");
+        switch (curPath("url")) {
+          // switch (route()) {
+          case "/about":
+            return <About />;
+          case "/":
+            return <Home />;
+          // case "/topics":
+          //   return <Topics match={curPath.get()} />;
+          default:
+            if (curPath("url")?.startsWith("/topics"))
+              return <Topics basepath="/topics" match={curPath()} />;
+            else return "Wrong path 404";
+        }
+      })()}
+
+      {/* <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} /> */}
+
+      {/* <SimpleSwitch>
+        <SimpleSwitch.Case render={() => "Wrong path 404"} />
+        <SimpleSwitch.Case
+          path={"/topics"}
+          render={(props) => {
+            console.log(props);
+            return <Topics {...props} />;
+          }}
+        />
+        <SimpleSwitch.Case path={"/about"} render={() => <About />} />
+        <SimpleSwitch.Case path={"/"} render={() => <Home />} />
+      </SimpleSwitch> */}
+    </div>
+  );
+}
+
+// for my state
+registerCallback(forceUpdate);
+
+const root = document.getElementById("root-vdom");
+mount(root, () => <App />);
