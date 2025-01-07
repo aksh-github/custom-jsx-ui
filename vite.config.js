68ba0143c81498d1b1e8a8facf1c366bd290261e
Akshay B <beatle02@gmail.com>
2025-01-06 21:57:18 -0500

    Using env

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,40 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  esbuild: {
-    jsxFactory: "h",
-    jsxFragment: "df",
-    // jsxFactory: "domv2",
-    // jsxFragment: "domv2",
-  },
-  server: {
-    proxy: {
-      "/api/data-ts": {
-        target:
-          "https://raw.githubusercontent.com/aksh-github/pages/refs/heads/content/data/sanskrit/db/data-ts.json",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api\/data-ts/, ""),
-      },
-      "/api/data": {
-        target:
-          // "https://raw.githubusercontent.com/aksh-github/pages/refs/heads/content/data/sanskrit/db/data-ts.json",
-          "https://raw.githubusercontent.com/aksh-github/pages/refs/heads/content/data/sanskrit/db/",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => {
-          console.log(path);
-          // const urlparts = path.split("/");
-          // console.log(urlparts);
-          return path.replace(/^\/api\/data/, "");
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  // console.log(env);
+  // target api url should be github (for local) or whatever server is used
+
+  return {
+    esbuild: {
+      jsxFactory: "h",
+      jsxFragment: "df",
+      // jsxFactory: "domv2",
+      // jsxFragment: "domv2",
+    },
+    server: {
+      proxy: {
+        "/api/data-ts": {
+          target: `${env.VITE_API_URL}data-ts.json`,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api\/data-ts/, ""),
+        },
+        "/api/data": {
+          target:
+            // "${env.VITE_API_URL}data-ts.json",
+            `${env.VITE_API_URL}`,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => {
+            console.log(path);
+            // const urlparts = path.split("/");
+            // console.log(urlparts);
+            return path.replace(/^\/api\/data/, "");
+          },
         },
       },
     },
-  },
+  };
 });
