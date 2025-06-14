ff324e682e56f6245d82aba71ece155c831bbca0
Akshay B <beatle02@gmail.com>
2025-06-14 14:39:44 +0530

    new vite config for lib creation

---
+++ lib-vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/utils/vdom/vdom-lib.js", // or vdom-ai.js if that's your main file
+      name: "Microframe",
+      fileName: (format) => `microframe.${format}.js`,
+      formats: ["es", "umd"],
+    },
+    outDir: "dist-lib",
+    minify: true,
+  },
+});
