fe2427289120496da99fa6f5bcc7951f2ede93a7
Akshay B <beatle02@gmail.com>
2025-07-14 11:40:24 +0530

    exam implementation

--- src/only-js2.js
+++ src/only-js2.js
@@ -1,164 +1,146 @@
-function h(type, props, ...children) {
-  // Mark fragment nodes with a special prop
+// state lib
 
-  return {
-    type,
-    props: { ...(props || {}) },
+// Simulate React state
 
-    children: children.flat().map((child) => child),
+// let state = [];
+// let setters = [];
+// let currentIndex = 0;
+
+// function render() {
+//   currentIndex = 0;
+//   // Simulate re-rendering the component
+//   console.log("currentIndex reset");
+// }
+
+// function createState(initialValue) {
+//   const index = currentIndex;
+
+//   if (state[index] === undefined) {
+//     state[index] = initialValue;
+//   }
+
+//   const setState = (newValue) => {
+//     state[index] =
+//       typeof newValue === "function" ? newValue(state[index]) : newValue;
+//   };
+
+//   setters[index] = setState;
+//   currentIndex++;
+
+//   return [state[index], setState];
+// }
+
+// // Usage
+
+// function Odd() {
+//   const [count, setCount] = createState(1);
+
+//   console.log(`Odd: ${count}`);
+//   setCount(count + 2);
+// }
+
+// function Even() {
+//   const [count, setCount] = createState(0);
+
+//   console.log(`Even: ${count}`);
+//   setCount(count + 2);
+// }
+
+// function Counter() {
+//   const [count, setCount] = createState(0);
+//   const [s, setS] = createState("akshay");
+
+//   // const increment = () => setCount(count + 1);
+//   setCount(count + 1);
+//   setS("bapaye");
+
+//   console.log(count, s);
+
+//   if (count % 2 === 0) {
+//     Even();
+//   } else {
+//     Odd();
+//   }
+// }
+
+// // Simulate React rendering
+// Counter();
+// // after this we need to call render()
+// render();
+// Counter();
+// // after this we need to call render()
+// render();
+// Counter();
+
+// Odd();
+// Odd();
+
+// a variation to react state based on key
+
+const gs = {};
+
+const state = (_key, iv) => {
+  const key = _key;
+  let st = gs[key] || iv;
+
+  if (gs[key] == undefined) gs[key] = st;
+
+  const get = () => {
+    return st;
   };
+
+  const set = (nv) => {
+    gs[key] = nv;
+  };
+
+  return [gs[key], set];
+};
+
+// function C() {
+//   const [c, sc] = state("C", 0);
+
+//   sc(c + 1);
+
+//   console.log(c);
+// }
+
+// C();
+// C();
+// C();
+
+function Odd() {
+  const [count, setCount] = state("Odd0", 1);
+
+  console.log(`Odd: ${count}`);
+  setCount(count + 2);
 }
 
-// create a function that uses linked list from jsx
+function Even() {
+  const [count, setCount] = state("Even0", 0);
 
-function jsxToLinkedList(vnode) {
-  // Helper to create a linked list node with firstChild and next
-  function createNode(value) {
-    return { value, firstChild: null, next: null };
-  }
-
-  // Recursively convert vnode tree to linked list with firstChild/next
-  function traverse(node) {
-    if (node == null) return null;
-
-    // If node is an array, flatten and link each child via next
-    if (Array.isArray(node)) {
-      let head = null,
-        current = null;
-      for (const child of node) {
-        const childNode = traverse(child);
-        if (childNode) {
-          if (!head) {
-            head = childNode;
-            current = head;
-          } else {
-            current.next = childNode;
-            current = current.next;
-          }
-          // Move to the end of the current child's chain
-          while (current && current.next) current = current.next;
-        }
-      }
-      return head;
-    }
-
-    // If node is a primitive, wrap in a linked list node
-    if (
-      typeof node === "string" ||
-      typeof node === "number" ||
-      typeof node === "boolean" ||
-      node === undefined ||
-      node === null
-    ) {
-      return createNode(node);
-    }
-
-    // If node is a vnode object
-    const linkedNode = createNode({
-      type: node.type,
-      props: node.props,
-    });
-
-    // children will be a linked list via firstChild/next
-    if (node.children && node.children.length > 0) {
-      linkedNode.firstChild = traverse(node.children);
-    }
-
-    return linkedNode;
-  }
-
-  return traverse(vnode);
+  console.log(`Even: ${count}`);
+  setCount(count + 2);
 }
 
-function createElement(vnode) {
-  // Support function components
-  if (typeof vnode === "function") {
-    return createElement(vnode());
-  }
-  // Support function as type (function component)
-  if (vnode && typeof vnode.type === "function") {
-    return createElement(
-      vnode.type({ ...(vnode.props || {}), children: vnode.children })
-    );
-  }
-  // Support fragments: type === React.Fragment or type === undefined or type === null or type === ""
-  if (
-    vnode &&
-    (vnode.type === "" ||
-      vnode.type === Symbol.for("vdom.fragment") ||
-      vnode.type === "fragment")
-  ) {
-    const frag = document.createDocumentFragment();
-    (vnode.children || []).forEach((child) => {
-      frag.appendChild(createElement(child));
-    });
-    return frag;
-  }
-  if (vnode === null || vnode === undefined || typeof vnode === "boolean") {
-    return document.createTextNode("");
-  }
-  if (typeof vnode === "string" || typeof vnode === "number") {
-    return document.createTextNode(vnode);
-  }
+function Comp() {
+  const [c, setc] = state("Comp0", 0);
+  const [s, sets] = state("Comp1", "akshay");
+  // console.log(c)
+  setc(c + 1);
+  sets("bapaye");
 
-  if (typeof vnode.type !== "string") {
-    throw new Error("Invalid node type: " + vnode.type);
-  }
+  console.log(c, s);
 
-  const el = document.createElement(vnode.type);
-  // Set props/attributes
-  for (const [key, value] of Object.entries(vnode.props || {})) {
-    if (key === "className") {
-      el.className = value;
-    } else {
-      el.setAttribute(key, value);
-    }
+  if (c % 2 === 0) {
+    Even();
+  } else {
+    Odd();
   }
-
-  // Append children
-  (vnode.children || []).forEach((child) => {
-    el.appendChild(createElement(child));
-  });
-  return el;
 }
 
-function Food({ name, price }) {
-  return h(
-    "div",
-    { className: "food-item" },
-    h("h2", null, name),
-    h("p", null, `Price: $${price}`)
-  );
-}
+Comp();
+Comp();
+Comp();
+Comp();
 
-// Example usage:
-const vdom = h(
-  "div",
-  { id: "main" },
-
-  h("h1", { className: "title" }, "Hello World"),
-  h("p", { className: "description" }, "This is a description."),
-  h("span", null, "this is virtual World"),
-  h(Food, { name: "Pizza", price: 9.99 })
-);
-
-console.log(vdom, jsxToLinkedList(vdom));
-
-// Example usage:
-document.body.appendChild(createElement(vdom));
-
-const fragVdom = h(
-  "", // or use "fragment"
-  null,
-  h("span", null, "A"),
-  h("h2", null, "B"),
-  h("div", null, 0),
-  h("span", null, undefined),
-  h("span", null, null),
-  h("span", null, true),
-  h("span", null, false)
-  // h("span", null, { a: 10 })
-);
-
-console.log(fragVdom);
-document.body.appendChild(createElement(fragVdom));
+console.log(gs);
