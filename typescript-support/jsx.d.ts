8b8ceb76da6d43792ebdb6009a48d7b5646653f5
Akshay B <beatle02@gmail.com>
2026-01-04 10:50:35 +0530

    Update jsx.d.ts

--- typescript-support/jsx.d.ts
+++ typescript-support/jsx.d.ts
@@ -2,4 +2,21 @@ declare namespace JSX {
   interface IntrinsicElements {
     [elemName: string]: any;
   }
+
+  type Element =
+    | {
+        type: string;
+        props: any;
+        children: any[];
+        key?: string | undefined;
+        value?: any;
+        $t?: number;
+        $c?: string;
+        $p?: string;
+      }
+    | null
+    | undefined
+    | string
+    | number
+    | boolean;
 }
