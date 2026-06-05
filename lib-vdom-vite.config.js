4ac713228c285f5d8271b18eaa5e7c25906608af
Akshay B <beatle02@gmail.com>
2026-06-05 22:41:21 +0530

    aliases added, build script for dom

--- lib-vdom-vite.config.js
+++ lib-vdom-vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@vdom-lib": "/src/utils/vdom/vdom-lib.js",
+      "@dom-lib": "/src/utils/dom/dom-lib.js",
+      "@simple-signal": "/src/utils/signal-v2.js",
+    },
+  },
   build: {
     lib: {
-      entry: "./src/utils/vdom/vdom-lib.js", // or vdom-ai.js if that's your main file
+      entry: "./src/utils/dom/dom-lib.js", // or vdom-ai.js if that's your main file
       name: "Microframe",
       fileName: (format) => `microframe.${format}.[hash:8].js`,
       formats: ["es", "umd"],
