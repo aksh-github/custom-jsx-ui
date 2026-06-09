36cfe516dae95d44b78da8810149856cc0567f81
Akshay B <beatle02@gmail.com>
2026-06-09 23:35:41 +0530

    VVV Imp: Alias intro, using new key goog lib

--- lib-vdom-vite.config.js
+++ lib-vdom-vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
+import { resolve } from "path";
 
 export default defineConfig({
+  // plugins: [
+  //   visualizer({
+  //     emitFile: true,
+  //     filename: "stats.html",
+  //   }),
+  // ],
+  resolve: {
+    alias: {
+      // "@vdom-lib": "/src/utils/vdom/vdom-lib.js",
+      "@vdom-lib": "/src/utils/vdom/vdom-key-lib-goog.js",
+      // "@simple-state": "/src/lib/simple-state.js",
+      "@vdom-ssr": "/src/utils/vdom/vdom-ssr.js",
+      "@router-v2": "/src/utils/router-v2.jsx",
+    },
+  },
   build: {
     lib: {
-      entry: "./src/utils/vdom/vdom-lib.js", // or vdom-ai.js if that's your main file
+      // entry: "./src/utils/vdom/vdom-lib.js", // or vdom-ai.js if that's your main file
+      entry: "./src/utils/vdom/vdom-key-lib-goog.js",
       name: "Microframe",
-      fileName: (format) => `microframe.${format}.[hash:8].js`,
+      fileName: (format) => `microframe.${format}.goog.[hash:8].js`,
       formats: ["es", "umd"],
     },
     outDir: "dist-lib",
