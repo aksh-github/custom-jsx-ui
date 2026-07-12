b0603990ccd3ee42cc293a7e07bb81932dd53fd4
Akshay B <beatle02@gmail.com>
2026-07-12 23:24:24 +0530

    Display something

--- src/compos/ServerData.jsx
+++ src/compos/ServerData.jsx
@@ -2,13 +2,13 @@ import { h } from "@vdom-lib";
 
 export function ServerData(props) {
   //   console.log(props);
-  if (!props.data) return null;
+  // if (!props.data) return null;
 
   return (
     <section>
       <h1>Server Data Example</h1>
       <ul>
-        {props.data.resp.map((item, idx) => {
+        {props?.data?.resp.map((item, idx) => {
           return <li key={`k${item.id}`}>{item.title}</li>;
         })}
       </ul>
